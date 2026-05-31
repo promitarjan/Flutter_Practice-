06830cb91cd3761c8b1c2b78186f35d235a73cd5
promitarjan <arjanpromit@gmail.com>
2026-05-31 21:34:46 +0600

    Updated the UI

--- Visiting_Card/lib/home_screen.dart
+++ Visiting_Card/lib/home_screen.dart
@@ -13,7 +13,42 @@ class BusinessCardScreen extends StatelessWidget {
           backgroundColor: Colors.deepPurpleAccent,
           centerTitle: true,
         ),
-        body: const Center(child: Text("This is the body")),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              children: [
+                SizedBox(height: 15),
+                Center(
+                  child: CircleAvatar(
+                    radius: 64,
+                    backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1494790108377-be9c29b29330",
+                    ),
+                  ),
+                ),
+                SizedBox(height: 12),
+                Text(
+                  "Ariana Rahman",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF1A103D),
+                  ),
+                ),
+                SizedBox(height: 1),
+                Text(
+                  "Flutter Developer & UI Designer",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: const Color.fromARGB(255, 137, 57, 228),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
