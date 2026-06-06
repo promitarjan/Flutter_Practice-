82c1ca56763c66cb4dd4b36f4d34a219dbcc0952
promitarjan <arjanpromit@gmail.com>
2026-06-06 19:08:03 +0600

    product showcase header updated

--- product_showcase_screen/lib/features/product_showcase/presentation/pages/product_showcase_header.dart
+++ product_showcase_screen/lib/features/product_showcase/presentation/pages/product_showcase_header.dart
@@ -5,48 +5,78 @@ class ProductShowcaseHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
 
-      children: [
-        SizedBox(height: 12),
-        Row(
-          children: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.menu_rounded)),
-            const Spacer(),
-            Text(
-              'ShopNest',
-              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                fontWeight: FontWeight.w800,
-                color: const Color(0xFF172554),
+        children: [
+          SizedBox(height: 12),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.menu_rounded),
               ),
-            ),
-            Spacer(),
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.shopping_cart_outlined),
+              const Spacer(),
+              Text(
+                'ShopNest',
+                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                  fontWeight: FontWeight.w800,
+                  color: const Color(0xFF172554),
                 ),
-                Positioned(
-                  right: 6,
-                  top: 4,
-                  child: Container(
-                    height: 18,
-                    width: 18,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D4ED8),
-                      shape: BoxShape.circle,
+              ),
+              Spacer(),
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.shopping_cart_outlined),
+                  ),
+                  Positioned(
+                    right: 6,
+                    top: 4,
+                    child: Container(
+                      height: 18,
+                      width: 18,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF1D4ED8),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Text(
+                        "2",
+                        style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 11,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 24),
+          Text(
+            "Featured Products",
+            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+              fontWeight: FontWeight.w700,
+              color: const Color(0xFF020617),
             ),
-          ],
-        ),
-      ],
+          ),
+          const SizedBox(height: 14),
+          Text(
+            "Discover trending picks",
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              fontWeight: FontWeight.w500,
+              color: const Color(0xFF64748B),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
